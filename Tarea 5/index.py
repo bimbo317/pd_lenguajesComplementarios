716b7120bfcf963dab888de9d948efd6ef197399
Bimbo317 <bimpo2002@hotmail.com>
2022-10-13 00:04:54 -0300

    web app con layout

---
+++ Tarea 5/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template  # llamar al framework
+
+app = Flask(__name__)  # guarda en una variable la ruta de inicio de la app
+
+# Rutas de procesamiento (direccionan a algun lugar)
+@app.route('/')    # método que crea una url
+def home():        # función  que devuelve información al navegador
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+# validamos si estamos en el archivo principal para que siempre se quede
+# escuchando una peticion del usuario y si se cumple ejecuta el app.run
+if __name__ == '__main__':
+    app.run(debug=True)
